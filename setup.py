03f9e09a5230f8bc9d937d1855c736fce1c4eed2
Mac Ryan <quasipedia@gmail.com>
2014-10-16 18:48:39 +0200

    Second tentative at building a wheel

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
+
+    # Authoring
     name='swaggery',
     version='1.0.0a1',
     description='A Python3 framework to create self-documenting swagger APIs',
@@ -19,7 +21,8 @@ setup(
     author_email='quasipedia@gmail.com',
     license='AGPLv3+',
     keywords='swagger API documentation asyncronous framework uWSGI WSGI',
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+    # Classifiers: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'License :: OSI Approved :: '
             'GNU Affero General Public License v3 or later (AGPLv3+)',
@@ -34,16 +37,17 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
 
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    # Dependencies
     install_requires=[
         'uWSGI==2.0.7',
         'Werkzeug==0.9.6',
         'jsonschema==2.4.0',
+        'docopt==0.6.2',
     ],
     extras_require={
         'dev': [
-            'docopt==0.6.2',
             'wheel==0.24.0',
+            'pip==1.5.6',
         ],
         'test': [
             'coverage==3.7.1',
@@ -51,16 +55,14 @@ setup(
             'rednose==0.4.1',
         ],
     },
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. see
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-
-    # additional-files In this case, 'data_file' will be installed into
-    # '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
+    # Content
+    py_modules=['wsgi_application'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data={
+        'swaggery': ['static/*'],
+    },
+    data_files=[('examples', ['examples/*'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
